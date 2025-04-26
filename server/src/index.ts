ea3a7b6ae7f7e8a3b53eb3b5ad8e730009206334
BAHIRABDULLA <bahirk880@gmail.com>
2025-04-26 15:05:34 +0530

    backend created

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+const app = express()
+import ocrRoute from './routes/ocr.routes'
+import cors from 'cors'
+
+const corsOption = {
+    origin:'http://localhost:5173',
+    credential:true
+}
+app.use(cors(corsOption))
+app.use('/',(req,res,next)=>{
+    console.log(req.url,'req.url');
+    next()    
+})
+app.use('/',ocrRoute)
+
+app.listen(3000,()=>console.log('server listening on http://localhost:3000'))
